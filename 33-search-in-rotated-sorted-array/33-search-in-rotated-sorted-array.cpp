c0078296f6ca26ad4990b44fa8164f04a1822384
Suraj Vatsya <surajkumar742494@gmail.com>
2022-03-01 12:11:09 +0530

    Time: 7 ms (44.88%), Space: 11.2 MB (30.42%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int n=nums.size();
+        int pivot;
+        int s=0;
+        int e = n-1;
+        
+        //find pivot
+        
+        while(s<=e){
+            int m= e+(s-e)/2;
+            if(nums[m]==target)
+                return m;
+           else if( nums[m]>=nums[s])
+            {
+                if(target<=nums[m] and target>=nums[s])
+                    e=m-1;
+               else
+                   s=m+1;
+                
+            }
+            else {
+                if(target<=nums[e] and target>=nums[m])
+                      s=m+1;
+                else
+                     e = m-1;
+                 
+            }
+        }
+        return -1;
+        
+    }
+};
