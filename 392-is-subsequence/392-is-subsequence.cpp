185cf4296653b5a256416c276125b70df372683b
Suraj Vatsya <surajkumar742494@gmail.com>
2022-03-02 07:17:03 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (72.95%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    
-    bool sove(string s, string t, int m, int n){
-        if(m==0)
-            return true;
-        if(n==0)
-            return false;
-        if(s[m-1]==t[n-1])
-            return sove(s,t,m-1,n-1);
-        
-       return  sove(s,t,m,n-1);
-        
-        
-    }
-    
-    
     bool isSubsequence(string s, string t) {
-      
         int m = s.length();
         int n = t.length();
         if(m>n)
             return false;
         
+        int i=0; int j = 0;
+        while(i<m and j<n){
+            
+            if(i==m )
+                return true;
+            
+            if(s[i]==t[j]){
+                i++;j++;
+            }
+            else
+                j++;
+        }
+        if(i==m)
+            return true;
+        return false;
+            
         
-        return sove(s,t,m,n);
+        
     }
 };
