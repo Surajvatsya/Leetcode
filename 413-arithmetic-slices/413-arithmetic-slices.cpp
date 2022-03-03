a495b80834b55580be6f95bef00a1faa8f733a7a
Suraj Vatsya <surajkumar742494@gmail.com>
2022-03-03 18:34:14 +0530

    Time: 4 ms (49.49%), Space: 7.3 MB (88.02%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -5,16 +5,26 @@ public:
         int n = v.size();
         if(n<3)
             return 0;
-        vector<int>dp(n,0);
+        // vector<int>dp(n,0);
           long sum = 0;
+        // for(int i=2; i<n; i++){
+        //     if(v[i]-v[i-1] == v[i-1]-v[i-2])
+        //         dp[i]=dp[i-1]+1;
+        //     sum+=dp[i];
+        // }
+        // return sum;
+        
+        int cnt = 0;
         for(int i=2; i<n; i++){
             if(v[i]-v[i-1] == v[i-1]-v[i-2])
-                dp[i]=dp[i-1]+1;
-            sum+=dp[i];
+            {
+                  cnt++;
+                sum+=cnt;
+            }
+            else
+                cnt=0;
         }
-        
-      
-       
         return sum;
     }
+        
 };
