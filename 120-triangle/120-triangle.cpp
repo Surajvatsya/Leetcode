f2453fe869cbde62cb9d9de48056038bf13681df
Suraj Vatsya <surajkumar742494@gmail.com>
2022-03-04 01:07:06 +0530

    Time: 12 ms (30.57%), Space: 8.5 MB (89.89%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,23 +2,13 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& t) {
         int n = t.size();
-        int m = t[n-1].size();
-        
-        
-        int dp[n][m];
-        for(int i = n-1; i>=0; i--){
-            for(int j = t[i].size()-1; j>=0; j--){
-                if(i==n-1){
-                    dp[i][j]=t[i][j];
-                }
-                else{
-                    dp[i][j] = t[i][j]+min(dp[i+1][j+1],dp[i+1][j]);
-                }
+        vector<int>v = t[n-1];
+        for(int i= n-2; i>=0; i--){
+            for(int j=0; j<t[i].size(); j++){
+                v[j] = t[i][j] + min(v[j],v[j+1]); 
             }
         }
-        return dp[0][0];
-        
-        
+        return v[0];
         
     }
 };
