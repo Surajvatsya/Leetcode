f44729e96eb0f17305cb279447751e7252ca33ca
Suraj Vatsya <surajkumar742494@gmail.com>
2022-03-03 18:19:51 +0530

    Time: 7 ms (20.58%), Space: 7.5 MB (16.43%) - LeetHub

---
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& v) {
+        
+        int n = v.size();
+        if(n<3)
+            return 0;
+        vector<int>dp(n,0);
+        
+        for(int i=2; i<n; i++){
+            if(v[i]-v[i-1] == v[i-1]-v[i-2])
+                dp[i]=dp[i-1]+1;
+        }
+        
+        long sum = 0;
+        
+        for(auto &x : dp)
+            sum+=x;
+        return sum;
+    }
+};
