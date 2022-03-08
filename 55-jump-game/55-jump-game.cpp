8dba18896b876d0d2e162ca9d3947cc9c14deb4c
Suraj Vatsya <surajkumar742494@gmail.com>
2022-03-08 23:56:54 +0530

    Time: 133 ms (29.15%), Space: 48.3 MB (87.81%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+
+    //kis index tak jump kr skte h
+        //concept -> index pe khela
+    int can_jump = 0;
+        int n = nums.size();
+        for(int i=0; i<=can_jump; i++){
+            if(i==n-1)
+                return true;
+            can_jump = max(can_jump, i+nums[i]);
+        }
+        return false;
+    }
+};
