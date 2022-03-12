5937eedd05748a71e83d121af5ab92bd4efae88c
Suraj Vatsya <surajkumar742494@gmail.com>
2022-03-12 15:14:41 +0530

    Time: 9 ms (68.97%), Space: 11.3 MB (60.48%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,25 +17,48 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-      if(!head)
-          return head;
-        unordered_map<Node*, Node*>mp;
-        
-        Node* temp = head;
-        while(temp){
-            Node* newNode = new Node(temp->val);
-           mp[temp]= newNode;
-            temp=temp->next;
-        }
-        mp[NULL] = NULL;
-        temp = head;
-        
-        while(temp){
-            mp[temp]->next = mp[temp->next];
-            mp[temp]->random = mp[temp->random];
-            temp=temp->next;
-        }
-        
-        return mp[head];
+        Node *iter = head; 
+          Node *front = head;
+
+          // First round: make copy of each node,
+          // and link them together side-by-side in a single list.
+          while (iter != NULL) {
+            front = iter->next;
+
+            Node *copy = new Node(iter->val);
+            iter->next = copy;
+            copy->next = front;
+
+            iter = front;
+          }
+
+          // Second round: assign random pointers for the copy nodes.
+          iter = head;
+          while (iter != NULL) {
+            if (iter->random != NULL) {
+              iter->next->random = iter->random->next;
+            }
+            iter = iter->next->next;
+          }
+
+          // Third round: restore the original list, and extract the copy list.
+          iter = head;
+          Node *pseudoHead = new Node(0);
+          Node *copy = pseudoHead;
+
+          while (iter != NULL) {
+            front = iter->next->next;
+
+            // extract the copy
+            copy->next = iter->next;
+
+            // restore the original list
+            iter->next = front;
+              
+            copy = copy -> next; 
+            iter = front;
+          }
+
+          return pseudoHead->next;
     }
 };
