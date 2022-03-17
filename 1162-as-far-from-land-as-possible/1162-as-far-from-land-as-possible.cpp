a1f79a756c4b45b3e71d729b709722f6bb7e830f
Suraj Vatsya <surajkumar742494@gmail.com>
2022-03-17 18:31:54 +0530

    Time: 81 ms (62.40%), Space: 19.8 MB (71.67%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,56 +1,50 @@
 class Solution {
 public:
-    int maxDistance(vector<vector<int>>& mat) {
-     int n = mat.size();
+    int maxDistance(vector<vector<int>>& grid) {
+        int n = grid.size();
         if(n==0)
             return 0;
         
-        int m = n;
-        queue<pair<int,int>> q;
-        
+        int level = -1;
+        int count = 0;
+        queue<pair<int,int>>q;
         for(int i=0; i<n; i++){
-            for(int j=0;j<n; j++){
-                if(mat[i][j]==1){
-                    mat[i][j]=0;
-                     q.push({i,j});
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==1)
+                {
+                    q.push({i,j});
+                    count++;
                 }
-                else{
-                    mat[i][j]=-1;
-                }
-                   
             }
         }
-        int ans = -1;
+        
+        if(count==n*n)
+            return -1;
+        
         
         while(!q.empty()){
-            int a = q.front().first;
-            int b = q.front().second;
-           
-            q.pop();
-            if( a+1<n  and  mat[a+1][b]==-1 ){
-                mat[a+1][b]= mat[a][b]+1;
-                q.push({a+1,b});
-                ans = max(ans, mat[a+1][b]);
+            int sz = q.size();
+            level++;
+            for(int i=0; i<sz; i++){
+               pair<int,int>p = q.front(); 
+                q.pop();
+                int x = p.first;
+                int y = p.second;
+                
+                int dx[] = {-1,1,0,0};
+                int dy[] = {0,0,1,-1};
+                for(int j=0; j<4; j++){
+                    int cx = x + dx[j];
+                    int cy = y + dy[j];
+                    if(cx>=0 and cy>=0 and cx<n and cy<n and grid[cx][cy]==0){
+                        grid[cx][cy]=grid[cx][cy] + 1;
+                        q.push({cx,cy});
+                    }
+                }
+                
             }
-             if( a-1>=0  and  mat[a-1][b]==-1){
-                mat[a-1][b]= mat[a][b]+1;
-                q.push({a-1,b}); 
-                  ans = max(ans, mat[a-1][b]);
-            }
-            if( b-1>=0 and mat[a][b-1]==-1  ){
-                mat[a][b-1]= mat[a][b]+1;
-                q.push({a,b-1}); 
-                 ans = max(ans, mat[a][b-1]);
-            }
-             if( b+1<m and mat[a][b+1]==-1 ){
-                 mat[a][b+1]= mat[a][b]+1;
-                q.push({a,b+1});
-                  ans = max(ans, mat[a][b+1]);
-            }
-            
         }
-        
-        return ans;
+        return level;
         
     }
 };
