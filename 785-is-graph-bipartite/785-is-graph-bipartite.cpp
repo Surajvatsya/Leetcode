08ccce24d78a0a9ff54a17b58b8307693197261e
Suraj Vatsya <surajkumar742494@gmail.com>
2022-04-07 21:11:55 +0530

    Time: 45 ms (23.01%), Space: 13.4 MB (93.42%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,76 +1,3 @@
-// // class Solution {
-// // public:
-// //     bool isBipartite(vector<vector<int>>& graph) {
-// //         int n = graph.size();
-// //         vector<int>color(n, -1);
-// //         //it'll work for visited array as well
-        
-// //         queue<int>q;
-// //         q.push(0);
-// //         int curr = 0;
-// //         color[0]=curr;
-// //         for(int i=1; i<n; i++){
-            
-// //             //cheacked here if I have visited it earlier
-// //             if(color[i]!=-1)
-// //                 continue;
-            
-// //             while(!q.empty()){
-// //                 int ele = q.front();
-// //                 q.pop();
-                    
-// //                     for(auto &x : graph[ele]){
-// //                         curr = 1 - curr;
-// //                         if(color[x]==-1)
-// //                         {
-// //                             color[x]=curr;
-// //                              q.push(x);
-// //                         }
-// //                          else if( color[x]!=curr)
-// //                             return false;
-// //                     }   
-// //                 }     
-// //             }
-// //         return true;
-// //     }
-// // };
-
-
-// class Solution {
-//  	public:
-//  		bool isBipartite(vector<vector<int>>& graph) {
-//  			int n = graph.size();
-           
-//  			vector<int>color(n, -1);
-//                     color[0] = 0;
-//  				queue<int>q;
-//  				q.push(0);
-
-//  			for (int i = 0; i < n; i++) {
-//  				if (color[i] != -1) continue;
-
- 				
-//  				while (!q.empty()) {
-//  					int t = q.front();
-//                     int curr = color[t];
-//  					q.pop();
-//                          curr = curr xor 1;
-//  					for (auto &x : graph[t]) {
-                        
-//  						if (color[x] == -1) {
-//  							color[x] = curr;
-//  							q.push(x);
-//  						}//color[x]!=-1 
-//                         else if (color[x] != curr) {
-//  							return false;
-//  						}
-//  					}
-//  				}
-//  			}
-
-//  			return true;
-//  		}
-//  	};
 
 class Solution {
  	public:
@@ -82,13 +9,16 @@ class Solution {
  				
  			for (int i = 0; i < n; i++) {
  				if (color[i] != -1) continue;
-                        q.push(i);
- 				color[i] = 0;
+                int curr = 0;
+                //for disconnected graph
+                q.push(i);
+                //initialise first ele of every component with 0 color
+ 				color[i] = curr;
 
  				while (!q.empty()) {
  					int t = q.front();
  					q.pop();
-
+            
  					for (auto &x : graph[t]) {
  						if (color[x] == -1) {
  							color[x] = 1-color[t];
