0bd77790d209425f86fd5f941b48ca40c88adc35
Suraj Vatsya <surajkumar742494@gmail.com>
2022-04-09 19:43:21 +0530

    Time: 64 ms (22.80%), Space: 19.9 MB (63.13%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -1,21 +1,35 @@
 class KthLargest {
 public:
-
-    priority_queue<int,vector<int>,greater<int>>q;
-    int size;
-    KthLargest(int k, vector<int>& nums) { 
-        size=k;
-        for(auto c:nums)
-        {
-            q.push(c);
-            if(q.size() > k) q.pop();
+    int k;
+    priority_queue<int, vector<int>, greater<int>>pq;
+    KthLargest(int K, vector<int>& nums) {
+        k=K;
+        for(int i=0; i<nums.size(); i++){
+            pq.push(nums[i]);
+            if(pq.size()>k){
+                pq.pop();
+            }
         }
+        
     }
     
     int add(int val) {
-        q.push(val);
-        if(q.size() > size) q.pop();
-        return q.top();
+        pq.push(val);
+            if(pq.size()>k){
+                pq.pop();
+            }
+        return pq.top();
     }
-	
 };
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
+
+
+// we have taken min-heap why??
+    
+//     k size ka hi heap rkhna h hmko since I want to extract kth largest
+//     element in O(1) time we use min heap
