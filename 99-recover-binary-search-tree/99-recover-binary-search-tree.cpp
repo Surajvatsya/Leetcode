d48c11cc7c9b9fc6c23b4d558334eefe51f5b938
Suraj Vatsya <surajkumar742494@gmail.com>
2022-04-20 02:06:01 +0530

    Time: 45 ms (52.62%), Space: 58 MB (47.16%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -11,31 +11,92 @@
  */
 class Solution {
 public:
-    vector<int> v;
+    TreeNode*pr = new TreeNode(INT_MIN);
+    TreeNode*first=NULL;
+    TreeNode*mid = NULL;
+    TreeNode*last = NULL;
     
-    void inorder(TreeNode* root){
-        if(!root)
-            return;
-        inorder(root->left);
-        v.push_back(root->val);
-        inorder(root->right);
-    }
-    void inorders(TreeNode* root){
-        if(!root)
-            return;
-        inorders(root->left);
-        if(v[0]!=root->val){
-            root->val = v[0];
+    
+    void solve(TreeNode* root){
+        TreeNode* curr = root;
+        
+        while(curr){
+            
+            // check left pointer 
+//             10
+ //              \
+//                 20
+                
+            if(curr->left == NULL){
+                
+                // inorder.push_back(curr->val);
+                if(!first and curr->val < pr->val){
+                    first = pr;
+                    mid = curr;
+                }
+                else if(!last and curr->val < pr->val)
+                    last = curr;
+                pr = curr;
+                    
+                
+                //no need to preserve curr
+                curr= curr->right;
+            }
+            else{
+                
+               //  10
+               // /    \
+               // 5
+               // \
+               //   6   20
+               // preserve curr -> b/c curr->left ka rightmost ko thread bnana h uske sath
+               TreeNode*prev = curr->left;
+                while(prev->right!=NULL and prev->right!=curr){
+                    prev = prev->right;
+                }
+                //check phle se to thread nhi h
+                if(prev->right==NULL){
+                    // 2
+                    // \
+                    // 3
+                    // \
+                    //  4
+                    //create thread
+                    prev->right = curr;
+                    //when we make thread we do 
+                    curr= curr->left;
+                }
+                else{
+                  //   2
+                  //  / \
+                  // 4   5
+                    prev->right = NULL;
+                    // inorder.push_back(curr->val);
+                     if(!first and curr->val < pr->val){
+                    first = pr;
+                    mid = curr;
+                }
+                 else if(!last and curr->val < pr->val)
+                    last = curr;
+                     pr = curr;
+                    //when we break thread we do 
+                    curr=curr->right;
+                }
+                
+                        
+            }
+                
         }
-        v.erase(v.begin());
-        inorders(root->right);
     }
     
     void recoverTree(TreeNode* root) {
-   
-        inorder(root);
-        sort(v.begin(), v.end());
-        inorders(root);
-      
+        // vector<int> inorder;
+          solve(root);
+        
+        if(!last)
+            swap(first->val , mid->val);
+        else
+            swap(first->val , last->val);
+        
     }
 };
