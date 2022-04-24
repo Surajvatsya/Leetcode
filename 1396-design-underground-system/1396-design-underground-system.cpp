3f7adeafc9c3342f80544b3de07ab5e4fd788762
Suraj Vatsya <surajkumar742494@gmail.com>
2022-04-24 20:27:20 +0530

    Time: 202 ms (56.09%), Space: 61.9 MB (5.76%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.cpp
+++ 1396-design-underground-system/1396-design-underground-system.cpp
@@ -1,8 +1,12 @@
 class UndergroundSystem {
 public:
     
-    
+    //id ko pair kiya    pair({starting point and time of start})
     unordered_map<int,pair<string,int>>mp; 
+    
+    
+    //jab ham phuch gye to start to end ek journey hua jiska string bna lye us string ko pair kr dya t2-t1 k sath where t2 = destination time and t1 = arrival time
+    // ab jitne v log us start point se end point tak jaynge sbka t2-t1 ko us string me push krte hue jana h
     unordered_map< string,vector<int> >mp1; 
     
     
@@ -16,9 +20,11 @@ public:
     
     void checkOut(int id, string stationName, int t) {
         pair<string,int> p = mp[id];
+        //whenever you make pair of strings do make " " seprate b/w them
+        //bcs a ab same as aa b without space aab aab
         string route = p.first+" "+stationName;
-        int dist = t  - p.second;
-        mp1[route].push_back(dist);
+        int t_diff = t  - p.second;
+        mp1[route].push_back(t_diff);
     }
     
     double getAverageTime(string startStation, string endStation) {
