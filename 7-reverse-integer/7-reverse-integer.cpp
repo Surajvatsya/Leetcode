7299c0d3099eb5f8bf18223b90dad54ddd9bab14
Suraj Vatsya <surajkumar742494@gmail.com>
2022-05-05 01:58:52 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (34.27%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -4,18 +4,26 @@ public:
     int reversed_no(int n){
         long ans = 0;
         
+        // Reverse Integer krna ho to yhi follow kro always
+        
         while(n){
             
+            // //if n<0 to v run krega
+            //  if(ans>INT_MAX/10 or ans< INT_MIN/10)
+            // b/c ans*10>INT_MAX/10 *10 or ans*10< INT_MIN/10 *10
+            //   return 0;
             
-            
+            // NOTE :- -ve no k modulo v -ve hota h
             int temp = n%10;
- 
             ans = (ans*10) + temp;
+          
+            //since yha ans*10 ho rha h so ans ko long me lye h
             
             if(ans>INT_MAX or ans< INT_MIN)
               return 0;
  
             n = n/10;
+
         }
         
        
