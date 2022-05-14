2a8528de6f6e9b8473d7db263061afe04475971a
Suraj Vatsya <surajkumar742494@gmail.com>
2022-05-14 13:17:31 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (80.00%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
     bool isValid(string s) {
-        
-        stack<char>st;
-        if(s.length()%2!=0)
-            return 0;
-       for(int i = 0;i<s.length();i++)
-       {
-           
-           if(s[i]=='{' or s[i]=='[' or s[i]=='(')
-           {
-               st.push(s[i]);
-           }
-           
-           else if(st.empty())
-               return false;
-           
-          else if(s[i]=='}')
-           {
-               if(st.top()!='{' )
-                   return false;
-               st.pop();
-           }
-           else if(s[i]==']' )
-           {
-               if(st.top()!='[' )
-                   return false;
-               st.pop();
-           }
-           else if(s[i]==')' )
-           {
-               if(st.top()!='(')
-                   return false;
-               st.pop();
-           }
-       }
-        return st.empty();
+        stack<char> c;
+        for(int i=0; i<s.size(); i++){
+            
+            if(s[i]=='{' or s[i]=='(' or  s[i]=='['){
+                c.push(s[i]);
+            }
+            
+            //think about } / )  / ] these test cases if closing comes first st empty h so if st is empty we cant access its top value so
+            
+          else  if(c.empty())
+                return false;
+            
+            if(s[i]=='}'){
+                if(c.top()!='{')
+                    return false;
+                   c.pop(); 
+            }
+             if(s[i]==')'){
+                if(c.top()!='(')
+                    return false;
+                     c.pop(); 
+            }
+             if(s[i]==']'){
+                if(c.top()!='[')
+                    return false;
+                    c.pop();  
+            }
+            
+        }
+       return c.empty(); 
     }
 };
