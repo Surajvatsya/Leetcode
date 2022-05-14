f8cd42cfaa5720a60dd2091a07faa0eb14488435
Suraj Vatsya <surajkumar742494@gmail.com>
2022-05-14 18:55:06 +0530

    Time: 8 ms (64.67%), Space: 8.7 MB (65.77%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        //map ka space complexcity = O(N)
-        // Islye make shorter array map
-         vector<int> ans(nums1.size(),-1);
-        unordered_map<int,int>mp;
+       
         
-        for(int i=0; i<nums1.size(); i++){
-            mp[nums1[i]]=i;
-            
-        }
+        //nums1 subset h, nums2 ka jitna v greater element h sabko nikal lo
+        //nums1 me jiska jiska maang rha h sbke samne rakh dena 
+        
+        unordered_map<int,int> mp;
         stack<int>st;
+        vector<int>ans(nums1.size(),-1);
         
         for(int i=0; i<nums2.size(); i++){
             
-            if(mp.find(nums2[i])!=mp.end()){
-                
-                if(st.empty() or  st.top()>nums2[i])
-                    st.push(nums2[i]);
-                
-                else{
-                     while(!st.empty() and st.top()<nums2[i]){
-                        ans[mp[st.top()]]=nums2[i];
-                        st.pop();
-                        }
-                st.push(nums2[i]);
-              }
-            }
-            else{
-                while(!st.empty() and st.top()<nums2[i]){
-                    ans[mp[st.top()]]=nums2[i];
-                    st.pop();    
-                    }
+            while(!st.empty() and st.top()<nums2[i]){
+                mp[st.top()] = nums2[i];
+                st.pop();
             }
             
+            st.push(nums2[i]);
         }
-        return ans;
         
+        for(int i=0; i<nums1.size(); i++){
+            
+            if(mp.find(nums1[i])!=mp.end()){
+                ans[i]=mp[nums1[i]];
+            }
+        }
+        
+        return ans;
     }
 };
