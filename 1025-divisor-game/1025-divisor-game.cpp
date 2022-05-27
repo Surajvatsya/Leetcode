086ce1d8df7c534913e7f0f080631cd95aee9426
Suraj Vatsya <surajkumar742494@gmail.com>
2022-05-27 10:56:24 +0530

    Time: 4 ms (31.82%), Space: 6 MB (34.95%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -2,28 +2,35 @@ class Solution {
 public:
     //practical copy page no 20
     //thora iterative socho
-  
+ 
+    
    
-    int helper(int n){
+    int helper(int n, int *a){
         //no next move if you got n==1
         if(n==1)
             return 0;
         
+        if(a[n]!=-1)
+            return a[n];
         //explore all possiblities until n unless we dont get any no where that                     player           is winning
         for(int i=1; i<n and n%i==0; i++){
-            if(helper(n-i)==0)
+            if(helper(n-i,a)==0)
                 //it means helper(i) is the winner bcs helper(i-1) can't  next move
-               return 1;
+               return a[n-i]=1;
         }
         
         
         //from 1 to n if I dont have any possiblities where helper(i-1)==0 it means                 helper(i)->player 1 is not winning helper(i-1) is winning
-        return 0;
+        return a[n]=0;
         
     }
     
     bool divisorGame(int n) {
-       if(helper(n))
+         int dp[1001];
+        for(int i=0; i<1001; i++)
+            dp[i]=-1;
+        
+       if(helper(n,dp))
            return true;
         return false;
     }
