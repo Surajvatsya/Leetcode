8c66fb7b6501f87d6b778372add550be0df456e6
Suraj Vatsya <surajkumar742494@gmail.com>
2022-06-13 19:24:32 +0530

    Time: 687 ms (23.57%), Space: 114.1 MB (54.10%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
-    int maximumUniqueSubarray(vector<int>& nums) 
-    {
-        int l=0, r=0; //define left and right pointers for sliding windows approach
-        
-        vector<int> sum_arr; //sum_arr stores sum of element of nums at index i;
-        sum_arr.push_back(0); //initially l and r at 0 so to consider element at index 0 we need to take 0 at index 0
-        
-        int sum=0, score=nums[0];
-        for(auto it:nums) //storing sum into sum_arr
-        {
-            sum += it;
-            sum_arr.push_back(sum);
+    int maximumUniqueSubarray(vector<int>& nums) {
+        int i=0;
+        int j=0;
+        int n = nums.size();
+        int sum = 0;
+        int mx = 0;
+        unordered_map<int,int>mp;
+
+        while(j<n){
+            mp[nums[j]]++;
+            sum+=nums[j];
+            if(mp.size()==j-i+1)
+            {
+                mx=max(mx,sum);
+            }
+            else if(mp.size()<j-i+1) {
+               
+                        while(mp.size()!=j-i+1)
+                        {
+                                     sum-=nums[i];
+                            
+                                    mp[nums[i]]--;
+                                    if(mp[nums[i]]==0)
+                                        mp.erase(nums[i]);
+                                    i++;
+
+                         }
+                   }
+               j++;
+              // mx=max(mx,sum);
+                
+              }
+
+            return mx;
         }
-        
-        unordered_map<int, int> mp; //stores element and their index
-        
-        while(r<nums.size()) //iterate till r pointer is lesser than nums' size 
-        {
-            if(mp.find(nums[r]) != mp.end()) l=max(l, mp[nums[r]]+1); //when repeated element come we move left pointer of the right of repeated element
-            mp[nums[r]] = r; //assigning index to the element
-            score = max(score, sum_arr[r+1]-sum_arr[l]); //get max score
-            r++; //move r by 1 
-        }
-        return score;
-        
-    }
 };
