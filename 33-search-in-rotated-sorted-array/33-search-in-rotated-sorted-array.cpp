5b37e95f677e3a8daa88e30d7788a2ef2bfed3c8
Suraj Vatsya <surajkumar742494@gmail.com>
2022-06-23 17:51:32 +0530

    Time: 2 ms (88.04%), Space: 11.2 MB (29.05%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -15,14 +15,14 @@ public:
                 return m;
            else if( nums[m]>=nums[s])
             {
-                if(target<=nums[m] and target>=nums[s])
+                if(target<nums[m] and target>=nums[s])
                     e=m-1;
                else
                    s=m+1;
                 
             }
             else {
-                if(target<=nums[e] and target>=nums[m])
+                if(target<=nums[e] and target>nums[m])
                       s=m+1;
                 else
                      e = m-1;
