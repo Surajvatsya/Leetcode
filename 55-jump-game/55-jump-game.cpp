7eaefb7bbb6ae0c9cfaca9fe91b10aad02a2d8b1
Suraj Vatsya <surajkumar742494@gmail.com>
2022-06-24 15:05:10 +0530

    Time: 143 ms (24.93%), Space: 48.4 MB (42.65%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-    //greedy algo
-    //kis index tak jump kr skte h
-    //concept -> index pe khela
-    int can_jump = 0;
-        int n = nums.size();
-        for(int i=0; i<=can_jump; i++){
+         int n = nums.size();
+        int curr = 0;
+        int Indexcango=0;
+        for(int i=curr; i<= Indexcango ; i++){
             if(i>=n-1)
                 return true;
-            can_jump = max(can_jump, i+nums[i]);
+            Indexcango = max(Indexcango, i+nums[i] );
         }
         return false;
     }
