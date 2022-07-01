12c3ea23d5844e2dc0b11983895020debc830d02
Suraj Vatsya <surajkumar742494@gmail.com>
2022-07-01 19:25:28 +0530

    Time: 8 ms (70.55%), Space: 7 MB (99.57%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,64 +1,69 @@
 class Solution {
 public:
-    
-    bool isValid(vector<string>&v, int x, int y, int n){
-        for(int i=0; i<=x; i++){
-            if(v[i][y]=='Q')
+    bool issafe(int row,int col,  vector<string>&v, int n){
+        //vertical
+        for(int i=0; i<row; i++){
+            if(v[i][col]=='Q')
                 return false;
         }
         
-         int row = x;
-        int col = y;
-        while(row>=0 and col>=0){
-            if(v[row][col]=='Q')
-                return false;
-            row--;
-            col--;
-        }
-         row = x;
-         col = y;
-        while(row>=0 and col<n){
-            if(v[row][col]=='Q')
-                return false;
-            row--;
-            col++;
-        }
         
+        
+        //diagonal left
+        int i=row; int j=col;
+            while(i>=0 and j>=0){
+                if(v[i][j]=='Q')
+                    return false;
+                i--;
+                j--;
+            }
+        
+        
+        
+        
+        //diagonal right
+         i=row;  j=col;
+        while(i>=0 and j<n){
+            if(v[i][j]=='Q')
+                    return false;
+                i--;
+                j++;
+        }
         return true;
-        
     }
-    
-    void solve(vector<vector<string>>&ans, vector<string>&v, int n, int x){
+    void solve(int n , vector<vector<string>>&ans,vector<string>&v,int x){
         //base case
         if(x==n)
         {
             ans.push_back(v);
             return;
         }
-        //check valid pos
+        
         
         for(int col=0; col<n; col++){
-            if(isValid(v,x,col,n)){
+            if(issafe(x,col,v, n)){
                 //do
                 v[x][col]='Q';
                 
                 //recurr
-                solve(ans,v,n,x+1);
+                solve(n, ans, v, x+1);
                 
                 //undo
                 v[x][col]='.';
             }
         }
-        
     }
     
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
-        vector<string>v(n,string(n,'.'));
-        int x=0;
-       
-        solve(ans,v,n,x);
-        return ans;
+        //ans 
+        vector<vector<string>>ans;
         
+        //vector of string which I'll put in ans
+        vector<string>v(n, string(n,'.'));
+        
+        //1st row where you will put queen
+        int x= 0;
+        solve(n, ans, v,x);
+        return ans;
     }
 };
