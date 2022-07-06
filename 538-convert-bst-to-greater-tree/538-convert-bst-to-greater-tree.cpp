20c8047c197f3d302cfcc33b4226d54c88e9fcdb
Suraj Vatsya <surajkumar742494@gmail.com>
2022-07-06 14:31:11 +0530

    Time: 45 ms (80.51%), Space: 33.4 MB (60.12%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -10,52 +10,17 @@
  * };
  */
 class Solution {
+    private:
+    long sum = 0;
 public:
-    
-    void inorder(TreeNode* root,  vector<int> &v){
-        if(!root)
-            return ;
-       
-        //inorder left
-        inorder(root->left, v);
-        //business
-        v.push_back(root->val);
-        //right
-        inorder(root->right, v);
-    }
-    void change(TreeNode* root,  vector<int> &v){
-        if(!root)
-            return ;
-        // cout<<1;
-        //inorder left
-        change(root->left, v);
-        //business
-           root->val = v[0];
-           v.erase(v.begin() + 0);
-        //right
-        change(root->right, v);
-         
-    }
-    
-    
     TreeNode* convertBST(TreeNode* root) {
         if(!root)
             return root;
         
-        //find inorder
-        vector<int> v;
-        inorder(root,v);
-        
-        int n = v.size();
-       
-        long s=0;
-        for(int i=n-1; i>=0; i--){
-            s+=v[i];
-            v[i]=s;
-        }
-         // cout<<s;
-        
-        change(root,v);
+        convertBST(root->right);
+        sum+=root->val;
+        root->val = sum;
+        convertBST(root->left);
         return root;
     }
 };
