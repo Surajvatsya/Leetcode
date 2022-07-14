83fd8d7a690edc14c9e25999d4bd35e3815cffcf
Suraj Vatsya <surajkumar742494@gmail.com>
2022-07-14 20:45:17 +0530

    Time: 5 ms (23.08%), Space: 7.7 MB (47.09%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
+    
+    int solve(vector<int>& nums, int i,  vector<int>&v){
+        if(i>=nums.size())
+            return 0;
+        
+        if(v[i]!=-1)
+            return v[i];
+        
+        int b = INT_MIN;
+        
+        int a = nums[i] + solve(nums, i+2, v);
+        
+        if(i+1<nums.size())
+         b = nums[i+1] + solve(nums, i+3, v);
+       
+        return v[i] = max(a,b);
+       
+    }
+    
     int rob(vector<int>& nums) {
+       
+        int i=0;
         int n = nums.size();
-        //always think of edge case
-        if(n==1)
-            return nums[0];
-        vector<int>v(n,-1);
-        v[0]=nums[0];
-        v[1]=max(nums[0],nums[1]);
-        for(int i=2; i<n; i++){
-            v[i] = max(v[i-1],(nums[i]+v[i-2]));
-        }
-        return v[n-1];
+        vector<int>v(n+1,-1);
+     return   solve(nums, i, v);
         
     }
 };
