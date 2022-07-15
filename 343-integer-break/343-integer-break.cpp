4a4dc59b8eb6d17d705ba394bba8d2d2a7c9d17b
Suraj Vatsya <surajkumar742494@gmail.com>
2022-07-15 20:23:09 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (49.67%) - LeetHub

---
+++ 343-integer-break/343-integer-break.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    int solve(int n,  vector<int>&dp){
+        if(n==1)
+            return 1;
+        if(dp[n]!=-1)
+            return dp[n];
+        int maxi=0;
+        for(int i=1; i<n; i++){
+            int l = solve(n-i, dp)*i;
+            int r = (n-i)*i;
+            int val = max(l,r);
+            maxi = max(maxi, val);
+            
+        }
+        return dp[n] = maxi;
+    }
+    
+    
+    int integerBreak(int n) {
+        vector<int>dp(n+1, -1);
+         return solve(n, dp);
+    }
+};
