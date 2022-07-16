395833efa2240ea5d00731ffd7fcb831fc18e5fd
Suraj Vatsya <surajkumar742494@gmail.com>
2022-07-16 10:01:26 +0530

    Time: 6 ms (77.33%), Space: 7.9 MB (46.78%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     
-    int findOne(int i){
-        //convert to binary
-        int count=0;
-        while(i>0){
-            int temp =i%2;
-            if(temp)
-                count++;
-            i=i/2;
-        }
-        return count;
+    int count(int n){
+        return __builtin_popcount(n);
     }
     
     vector<int> countBits(int n) {
-        vector<int>v(n+1);
-        for(int i=0; i<n+1; i++){
-            v[i] = findOne(i);
+         vector<int>v(n+1,0);
+        for(int i=1; i<n+1; i++){
+            v[i] = count(i);
         }
         return v;
     }
