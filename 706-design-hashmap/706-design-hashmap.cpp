ee2b1d6bd3d61c1bb78f74e615355031801af22a
Suraj Vatsya <surajkumar742494@gmail.com>
2022-07-18 09:34:39 +0530

    Time: 150 ms (86.13%), Space: 52.2 MB (86.15%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,52 +1,65 @@
 class MyHashMap {
-public:
-    vector<list<pair<int,int>>>v;
-    int sz;
-    MyHashMap() {
-        sz=100;
-        v.resize(sz);
+    private:
+    vector< list< pair< int,int > >>v;
+    int n;
+     int index(int key)
+    {
+        return key%n;
     }
-    
-    int hash(int key){
-        return key%sz;
-    }
-    
     auto search(int key){
-        int i = hash(key);
-        auto  it = v[i].begin();
-        for(  it = v[i].begin(); it!=v[i].end(); it++){
-            if(it->first == key)
-                return it;
-        }
-        return it;
+        int i = index(key);
+        auto it =  v[i].begin();
+        for(it=v[i].begin(); it!=v[i].end(); it++)
+        if(it->first==key)
+            return it;
+         return v[i].end();   
     }
+    bool contains(int key){
+        int i = index(key);
+        if(search(key)!=v[i].end()){
+            return true;
+        }
+        return false;
+    }
+    
+public:
+    MyHashMap() {
+        n = 100;
+        v.resize(n);
+    }
+    
+   
+    
+    
     
     void put(int key, int value) {
-        int i = hash(key);
-        auto it = search(key);
-        if(it!=v[i].end())
+        //if it contains just change the value of that key
+        if(contains(key)){
+            auto it = search(key);
             it->second = value;
+        }
+        else{
+            int i = index(key);
+            v[i].push_back({key,value});
+        }
         
-        else
-        v[i].push_back({key,value});
+        //else push another node of key and value pair
     }
     
     int get(int key) {
-        int i = hash(key);
-        auto it = search(key);
-        if(it!=v[i].end()){
-            return it->second;
+        if(contains(key)){
+            auto it = search(key);
+            return it->second ;
         }
         else
             return -1;
     }
     
     void remove(int key) {
-        int i = hash(key);
-        auto it = search(key);
-        if(it!=v[i].end()){
-            v[i].erase(it);
-        }
+        if(!contains(key))
+            return;
+        int i = index(key);
+        v[i].erase(search(key));
     }
 };
 
