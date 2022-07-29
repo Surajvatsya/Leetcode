bba23b3b6fd76dc89bcb7d44ac162fd2ad9dbb09
Suraj Vatsya <surajkumar742494@gmail.com>
2022-07-29 11:54:48 +0530

    Time: 3 ms (38.13%), Space: 6.2 MB (43.28%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -2,20 +2,38 @@ class Solution {
 public:
     int scoreOfParentheses(string s) {
         stack<int>st;
-        int score = 0;
+       int score = 0;
         for(int i=0; i<s.length(); i++){
             if(s[i]=='(')
             {
-                 st.push(score);
-                  score = 0;
+                 st.push(-1);
+                 
             }
             else
             {
-                score = st.top() + max(2*score, 1);
-                st.pop();
+                if(st.top()==-1){
+                    st.pop();
+                    st.push(1);
+                }
+                else{
+                    int val = 0;
+                    while(st.top()!=-1){
+                        val+=st.top();
+                        st.pop();
+                    }
+                    st.pop();
+                     st.push(2*val);
+                }
+                
+                
             }
             
         }
+        while(!st.empty()){
+            score += st.top();
+            st.pop();
+        }
+            
         return score;
     }
 };
