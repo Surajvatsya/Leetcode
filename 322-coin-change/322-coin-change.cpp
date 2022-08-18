5314f963703b6fdd2e9b6f957f30a2b0095f2f04
Suraj Vatsya <surajkumar742494@gmail.com>
2022-08-18 12:01:20 +0530

    Time: 188 ms (30.50%), Space: 49.4 MB (11.31%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,38 +1,47 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
+    
+    //concept
+    
+    int solve(int amount, vector<int>& coins,  vector<vector<int>>&dp, int n ){
         
-        int n = coins.size();
-        int a = amount;
-        
-        int t[n+1][a+1];
-        
+        //initialization
         for(int i=0; i<n+1; i++){
-            for(int j=0; j<a+1; j++){
+            for(int j=0; j<amount+1; j++){
                 if(j==0)
-                    t[i][j]=0;
-                if(i==0)
-                    t[i][j]=INT_MAX-1;
+                    dp[i][j]=0;
                 
+              if(i==0)  
+                  dp[i][j] = INT_MAX-1;
             }
         }
+         for(int j=1; j<amount+1; j++){
+             if(j % coins[0] == 0  )
+                 dp[1][j] = j/coins[0];
+             else
+                 dp[1][j] = INT_MAX-1;
+         }
         
         
-            for(int j=1; j<a+1; j++){
-                if(j%coins[0]==0)
-                    t[1][j]=j/coins[0];
-                else
-                    t[1][j]=INT_MAX-1;
-            }
         for(int i=2; i<n+1; i++){
-            for(int j=1; j<a+1; j++){
+            for(int j=1; j<amount+1; j++){
                 if(coins[i-1]<=j)
-                    t[i][j]=min(1+t[i][j-coins[i-1]] , t[i-1][j]);
-                else
-                    t[i][j]=t[i-1][j];                
+                    dp[i][j] = min(1+dp[i][j-coins[i-1]], dp[i-1][j]);
+                
+              else
+                  dp[i][j] = dp[i-1][j];
             }
         }
-        return t[n][a]==INT_MAX-1?-1:t[n][a];
+        
+        return dp[n][amount]==INT_MAX-1?-1:dp[n][amount];
+        
+        
+    }
+    
+int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>>dp (n+1, vector<int>(amount+1));
+        return solve(amount, coins, dp, n);
         
     }
 };
