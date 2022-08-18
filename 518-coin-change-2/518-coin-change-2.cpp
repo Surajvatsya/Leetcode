46d21f951abace71f7617544bd98502bf1b157bc
Suraj Vatsya <surajkumar742494@gmail.com>
2022-08-18 12:14:40 +0530

    Time: 59 ms (33.70%), Space: 18.5 MB (38.29%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,30 +1,44 @@
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
-        //unbounded knapsack
+    
+    //concept
+    
+    int solve(int amount, vector<int>& coins,  vector<vector<int>>&dp, int n ){
         
-        int n = coins.size();
-        int t[n+1][amount+1];
-        
+        //initialization
         for(int i=0; i<n+1; i++){
             for(int j=0; j<amount+1; j++){
-                if(i==0)
-                    t[i][j]=0;
-                if(j==0)
-                    t[i][j]=1;
+               
+                
+              if(i==0)  
+                  dp[i][j] = 0 ;
+                
+                 if(j==0)
+                    dp[i][j]=1;
             }
         }
+        
+        
+         
         
         for(int i=1; i<n+1; i++){
             for(int j=1; j<amount+1; j++){
                 if(coins[i-1]<=j)
-                    t[i][j]=t[i][j-coins[i-1]] + t[i-1][j];
-                else
-                    t[i][j]=t[i-1][j];
+                    dp[i][j]= dp[i][j-coins[i-1]] + dp[i-1][j];
                 
+              else 
+                  dp[i][j] = dp[i-1][j];
             }
         }
         
-        return t[n][amount];
+        return dp[n][amount];
+        
+    }
+    
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>>dp (n+1, vector<int>(amount+1));
+        return solve(amount, coins, dp, n);
+        
     }
 };
