dc666aa055956d07300fa582a9f37648c2744edd
Suraj Vatsya <surajkumar742494@gmail.com>
2022-09-04 10:33:31 +0530

    Time: 8 ms (86.26%), Space: 9.4 MB (99.06%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -2,15 +2,6 @@ class Solution {
 public:
     
     
-    int power(long long int x, long long int y)
-{
-    if (y == 0)
-        return 1;
-    else if (y % 2 == 0)
-        return power(x, y / 2) * power(x, y / 2);
-    else
-        return x * power(x, y / 2) * power(x, y / 2);
-}
     
     int singleNumber(vector<int>& nums) {
         vector<int>ans(32,0);
@@ -23,7 +14,7 @@ public:
             if(nums[i]<0)
             {
                 a = a&INT_MAX;
-                long long b = 2147483648;
+                long long b = (long long )INT_MAX+1;
                 //2147483648
                 a=a | b;
             }
@@ -40,11 +31,10 @@ public:
          for(int i=0; i<ans.size(); i++){
             
              ans[i]=ans[i] % 3;
-              // cout<<ans[i];
+             
              
          }
-        // cout<<endl;
-        // long long val=0;
+        
         long long base = 1;
         int dec_val=0;
         for(int i=0; i<32; i++){
