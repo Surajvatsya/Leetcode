c0ddd591071746c3cbfd1edbba96e17aa3ec2211
Suraj Vatsya <surajkumar742494@gmail.com>
2022-09-26 15:54:41 +0530

    Time: 847 ms (41.82%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 
-select user_id, concat(upper(substr(name, 1, 1)),  LOWER(substr(name, 2, length(name)-1))  ) as name
-from users 
-order by user_id ASC
+select u.user_id, concat(upper(substr(u.name, 1, 1)),  LOWER(substr(u.name, 2, length(name)-1))  ) as name
+from users u
+order by u.user_id 
 ;
