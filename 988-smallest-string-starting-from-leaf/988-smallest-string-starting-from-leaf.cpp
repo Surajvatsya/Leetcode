077f68dc40343f704cd8b76167e48b56839a4d6c
Suraj Vatsya <surajkumar742494@gmail.com>
2022-10-04 21:06:05 +0530

    Time: 26 ms (32.72%), Space: 19.6 MB (76.48%) - LeetHub

--- 988-smallest-string-starting-from-leaf/988-smallest-string-starting-from-leaf.cpp
+++ 988-smallest-string-starting-from-leaf/988-smallest-string-starting-from-leaf.cpp
@@ -1,49 +1,46 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *   int val;
- *   TreeNode * left;
- *   TreeNode * right;
- *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        void solve(vector<vector < int>> &v, stack< int > st, TreeNode *root)
+class Solution {
+public:
+    void solve(string &prev, string curr, TreeNode* root){
+        if(!root)
+            return;
+        
+        curr=(char)( 'a'+root->val) + curr;
+        
+        if(!root->left and !root->right)
         {
-            if (!root)
-                return;
-
-            st.push(root->val);
-            
-            if (root->left == nullptr and root->right == nullptr)
-            {
-                vector<int> t;
-                while (!st.empty())
-                {
-                    t.push_back(st.top());
-                    st.pop();
-                }
-                v.push_back(t);
-                return;
+            if(prev.size()>0 ){
+                if(prev>curr)
+                    prev=curr;
+            }
+            else{
+                prev=curr;
             }
             
-             solve(v, st, root->left);
-             solve(v, st, root->right);
+            return;
         }
-
-    string smallestFromLeaf(TreeNode *root)
-    {
-        stack<int> st;
-        vector<vector < int>> v;
-        solve(v, st, root);
-        sort(v.begin(), v.end());
-        string s = "";
-        for(auto &x: v[0])
-            s+='a'+x;
-        return s;
+        
+         solve(prev, curr, root->left);
+         solve(prev, curr, root->right);
+        
+    }
+    
+    
+    string smallestFromLeaf(TreeNode* root) {
+        
+        string prev = "";
+        string curr = "";
+        solve(prev, curr, root);
+        return prev;
     }
 };
