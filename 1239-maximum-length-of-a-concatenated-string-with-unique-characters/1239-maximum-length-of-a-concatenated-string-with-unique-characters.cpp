e322c3c3f8a4812c00fcf66f750e0f2c708bb332
Suraj Vatsya <surajkumar742494@gmail.com>
2022-10-26 21:40:14 +0530

    Time: 119 ms (50.03%), Space: 58.1 MB (36.10%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,45 @@
+#define debug(x) cout<<#x<<" = "<<x<<endl;
+class Solution
+{
+    public:
+
+        bool valid(string s)
+        {
+            vector<int> v(26, 0);
+            for (auto &x: s)
+                v[x - 'a']++;
+            for (int i = 0; i < 26; i++)
+            {
+                if (v[i] > 1)
+                    return false;
+            }
+            return true;
+        }
+
+    void solve(vector<string> &arr, string s, int ind, int &count)
+    {
+       	//bc
+        if (ind >= arr.size())
+        {
+           // debug(s);
+            if (s.length() > count)
+                count = s.size();
+            return;
+        }
+
+       	//take
+        if(valid(s  + arr[ind] ))
+        solve(arr, s + arr[ind], ind + 1, count);
+
+       	//not take
+        solve(arr, s, ind + 1, count);
+    }
+
+    int maxLength(vector<string> &arr)
+    {
+        string s = "";
+        int count = 0;
+        solve(arr, s, 0, count);
+        return count;
+    }
+};
